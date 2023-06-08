5bfa1a206cbe7950d96d611266522b699a5ad96d
NablaT <remi.pourtier@gmail.com>
2023-06-08 23:38:24 +0200

    Adding environment variables for the backend and e2e tests

--- back-end/app/utils/base-model.js
+++ back-end/app/utils/base-model.js
@@ -11,8 +11,8 @@ module.exports = class BaseModel {
     if (!schema) throw new Error('You must provide a schema in constructor of BaseModel')
     this.schema = Joi.object().keys({ ...schema, id: Joi.number().required() })
     this.items = []
-    this.name = name
-    this.filePath = `${__dirname}/../../database/${this.name.toLowerCase()}.data.json`
+    this.name = name;
+    this.filePath = `${__dirname}/../../database/${process.env.DB_FOLDER ?? ''}${this.name.toLowerCase()}.data.json`
     this.load()
   }
 
